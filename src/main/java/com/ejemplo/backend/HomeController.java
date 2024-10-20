0a247dad50c6d957fba9ce03ffe11fd2d66d2d99
Juan <33juanpintos33@gmail.com>
2024-10-20 18:11:51 -0300

    Agregado conexion a HEROKU

--- src/main/java/com/ejemplo/backend/HomeController.java
+++ src/main/java/com/ejemplo/backend/HomeController.java
@@ -1,6 +1,7 @@
 package com.ejemplo.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class HomeController {
     }
 
     @GetMapping("/check-db")
+    @CrossOrigin(origins = "*")
     public String checkDatabaseConnection() {
         try (Connection connection = dataSource.getConnection()) {
             if (connection.isValid(1)) {
